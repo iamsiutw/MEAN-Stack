2dfc81d43126ee4c5db8a3bcad037880bb2dc323
yamsiu <mar6861214@gmail.com>
2018-11-06 16:58:47 +0800

    完成Angular前端與Express後端

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../post.service';
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: 'First title', content: 'The first content'},
+  //   { title: 'Second title', content: 'The second content' },
+  //   { title: 'Third title', content: 'The third content' },
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) { }
+
+  ngOnInit() {
+    this.postsService.getPosts();
+    this.postsService.getPostUpdatedListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
